720e9b46bff1a0d1740f8c5d1abdd1f111aea8ac
Alexander Minaku <al.motologa@gmail.com>
2023-01-22 21:08:20 +0200

    HW

---
+++ naming.py
@@ -0,0 +1,12 @@
+students_firstname = "John"
+students_lastname = "Doe"
+# denumirile compuse sunt separate vizual ( ex. drink_price sau executeAction() sau UserEnvironment )
+students_age = 31
+students_country_origin = "Germany"
+students_hometown = "Berlin"
+
+print("Prenumele studentului: ", students_firstname)
+print("Numele studentului: ", students_lastname)
+print("Varsta studentului: ", students_age)
+print("Tara de provenienta: ", students_country_origin)
+print("Orasul de bastina: ", students_hometown)
